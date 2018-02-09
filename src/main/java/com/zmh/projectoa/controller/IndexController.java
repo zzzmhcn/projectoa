fdc802382bc879719a5a0526ef8d13ccd9a6d550
张明辉 <361186482@qq.com>
2018-02-09 23:10:05 +0800

    换了高级的UI 希望别炸！

--- src/main/java/com/zmh/projectoa/controller/IndexController.java
+++ src/main/java/com/zmh/projectoa/controller/IndexController.java
@@ -2,11 +2,23 @@ package com.zmh.projectoa.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+/**
+ * @Author: ZMH 
+ * @Date: 2018/2/9 22:49
+ * 用于存放主页以及相关页面的Controller
+ */
 @Controller
 public class IndexController {
     @RequestMapping(value = "/index")
     public String index(){
         return "index";
+    }
+    @RequestMapping(value = "/calendar")
+    public String calendar(){
+        return "calendar";
+    }
+    @RequestMapping(value = "/form")
+    public String form(){
+        return "form";
     }
 }
--- src/main/java/com/zmh/projectoa/controller/LoginController.java
+++ src/main/java/com/zmh/projectoa/controller/LoginController.java
@@ -3,6 +3,11 @@ package com.zmh.projectoa.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * @Author: ZMH 
+ * @Date: 2018/2/9 22:48
+ * 主要存放登录用的Controller
+ */
 @Controller
 public class LoginController {
 
