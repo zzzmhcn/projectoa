9107e61baba38f5235fa3e68999858fb2e249024
张明辉 <361186482@qq.com>
2018-02-09 10:37:58 +0800

    Initial commit

---
+++ src/main/java/com/zmh/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.zmh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/")
+public class LoginController {
+
+    @RequestMapping(value = "/login")
+    public String login(){
+        return "login";
+    }
+}
---
+++ src/main/java/com/zmh/projectoa/ProjectoaApplication.java
@@ -0,0 +1,12 @@
+package com.zmh.projectoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectoaApplication.class, args);
+	}
+}
