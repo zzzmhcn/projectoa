38c3ecb81705088e9c8f4d5e41390247e9d07ed8
csyd <420120577@qq.com>
2018-02-09 14:23:22 +0800

    大笨蛋大笨蛋

--- src/main/java/com/zmh/projectoa/controller/LoginController.java
+++ src/main/java/com/zmh/projectoa/controller/LoginController.java
@@ -1,4 +1,4 @@
-package com.zmh.controller;
+package com.zmh.projectoa.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
