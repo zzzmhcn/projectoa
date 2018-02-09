3b3dcaa429bc092ca91888d6d586dfcc75e9843a
张明辉 <361186482@qq.com>
2018-02-09 21:10:43 +0800

    换了高级的UI 希望别炸！

--- src/main/java/com/zmh/projectoa/controller/LoginController.java
+++ src/main/java/com/zmh/projectoa/controller/LoginController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/")
 public class LoginController {
 
     @RequestMapping(value = "/login")
